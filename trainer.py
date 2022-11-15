11b209865c8cbdf4d2701a22383a288705b97884
lamhagoel <94916432+lamhagoel@users.noreply.github.com>
2022-11-15 16:53:08 -0600

    Fix syntax error

--- trainer.py
+++ trainer.py
@@ -37,7 +37,7 @@ class Trainer:
             #save_path=os.path.join(self.logger.writer.get_logdir(), "dynamics_model")
         #)
         self.logger.print("total time: {:.3f}s".format(time.time() - start_time))
-        torch.save(self.algo.dynamics_model.state_dict(),os.path.join(self.logger.writer.get_logdir(), "policyDynamics.pth")
+        torch.save(self.algo.dynamics_model.state_dict(),os.path.join(self.logger.writer.get_logdir(), "policyDynamics.pth"))
 
     def train_policy(self):
         start_time = time.time()
