c2f4c14513441e544a36b5d1088971be4033314a
Lamha Goel <lamhagoel@Lamhas-MacBook-Air.local>
2022-11-17 05:08:28 -0600

    Fix shape

--- algo/cql.py
+++ algo/cql.py
@@ -155,7 +155,7 @@ class CQLPolicy(nn.Module):
         sampled_q2 = torch.cat([sampled_q2, sampled_next_q2], dim=0) 
 
         #importance sampling
-        _random_log_prob = torch.ones(num_samples_for_estimation, actions.shape[0], 1).to(sampled_q1) * actions.shape[-1] * np.log(0.5)
+        _random_log_prob = torch.ones(num_samples_for_estimation*actions.shape[0], 1).to(sampled_q1) * actions.shape[-1] * np.log(0.5)
         sampling_weight = torch.cat([_random_log_prob, sampled_log_prob, _random_log_prob, sampled_next_log_prob], dim=0)
         sampled_q1 = sampled_q1 - sampling_weight
         sampled_q2 = sampled_q2 - sampling_weight
