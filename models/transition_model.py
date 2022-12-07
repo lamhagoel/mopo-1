f0ba579e6caa4779b6d942a9d88480d80a1fde67
Lamha Goel <lamhagoel@Lamhas-MacBook-Air.local>
2022-12-07 02:26:50 -0600

    Remove prints

--- models/transition_model.py
+++ models/transition_model.py
@@ -173,14 +173,14 @@ class TransitionModel:
         if penalty_coeff != 0:
             if self.penalty_type is None:
                 penalty = np.amax(np.linalg.norm(ensemble_model_stds, axis=2), axis=0)
-                print(str(penalty.shape) + " " + str(ensemble_model_stds.shape) + " " + str(rewards.shape))
+                # print(str(penalty.shape) + " " + str(ensemble_model_stds.shape) + " " + str(rewards.shape))
                 if not self.printed_penalty:
                     print("Default penalty - max variance norm")
                     self.printed_penalty = True
             elif self.penalty_type == "ind_var":
-                print(str(ensemble_model_stds.shape) + " " + str(ensemble_model_stds[model_idxes, batch_idxes, :].shape))
+                # print(str(ensemble_model_stds.shape) + " " + str(ensemble_model_stds[model_idxes, batch_idxes, :].shape))
                 penalty = np.linalg.norm(ensemble_model_stds[model_idxes, batch_idxes, :], axis=1)
-                print(str(penalty.shape) + " " + str(ensemble_model_stds.shape) + " " + str(rewards.shape))
+                # print(str(penalty.shape) + " " + str(ensemble_model_stds.shape) + " " + str(rewards.shape))
                 if not self.printed_penalty:
                     print("Penalty - ind variance norm")
                     self.printed_penalty = True
